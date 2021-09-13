69daf276caa53eede0a2cf574fbcc232988f35dc
Jingwen Li <865581101@qq.com>
2021-09-13 10:50:33 +0200

    testing on another machine

--- chatconllu/helpers/sentence.py
+++ chatconllu/helpers/sentence.py
@@ -1,52 +1,55 @@
 class Sentence(object):
-    """docstring for Sentence"""
+	"""docstring for Sentence"""
 
-    __slots__ = ['speaker',
-                 'tiers',
-                 'gra',
-                 'mor',
-                 'tokens',
-                 'clean',
-                 'comments',
-                 'sent_id',
-                 'toks',
-                ]
+	__slots__ = ['speaker',
+				 'tiers',
+				 'gra',
+				 'mor',
+				 'chat_sent',
+				 'clean',
+				 'comments',
+				 'sent_id',
+				 'toks',
+				]
 
-    def __init__(self,
-                 speaker=None,
-                 tiers=None,
-                 gra=None,
-                 mor=None,
-                 tokens=None,
-                 clean=None,
-                 comments=None,
-                 sent_id=None,
-                 toks=None
-                 ):
-        self.speaker=speaker
-        self.tiers=tiers
-        self.gra=gra
-        self.mor=mor
-        self.tokens=tokens
-        self.clean=clean
-        self.comments=comments
-        self.sent_id=sent_id
-        self.toks=toks
+	def __init__(self,
+				 speaker=None,
+				 tiers=None,
+				 gra=None,
+				 mor=None,
+				 chat_sent=None,
+				 clean=None,
+				 comments=None,
+				 sent_id=None,
+				 toks=None
+				 ):
+		self.speaker=speaker
+		self.tiers=tiers
+		self.gra=gra
+		self.mor=mor
+		self.chat_sent=chat_sent
+		self.clean=clean
+		self.comments=comments
+		self.sent_id=sent_id
+		self.toks=toks
 
-    def __str__(self):
+	def __str__(self):
 
-        fields = [str(getattr(self, x)) for x in self.__slots__[:]]
-        return ("\n".join(fields))
+		fields = [str(getattr(self, x)) for x in self.__slots__[:]]
+		return ("\n".join(fields))
 
-    def text(self):
-        tokens = [x.form for x in self.toks]
-        return " ".join(tokens)
+	def text(self):
+		tokens = [x.form for x in self.toks]
+		return " ".join(tokens)
 
-    def get_sent_id(self):
-        return self.sent_id
+	def get_sent_id(self):
+		return self.sent_id
 
-    def conllu_str(self):
-        s = ""
-        for tok in self.toks:
-            s += tok.conllu_str()
-        return s
+	def conllu_str(self, mute=False):
+		s = ""
+		for tok in self.toks:
+			if mute:
+				s += "# " + tok.conllu_str()
+			else:
+				s += tok.conllu_str()
+		return s
